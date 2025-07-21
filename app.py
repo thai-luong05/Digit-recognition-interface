66d5552ede11d9042d93b8e28d2d7de421ec6c70
Thai Luong <85025363+thai-luong05@users.noreply.github.com>
2025-07-20 18:01:59 -0700

    add parameter saving feature

--- app.py
+++ app.py
@@ -5,24 +5,32 @@ from PIL import Image
 from pathlib import Path
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import io
-import base64
+import io, os, base64
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
+MODEL_NAME = 'ann.npz' #path for saving parameters
 class ANN():
     def __init__(self):
-        n_in = 784 #28x 28
-        n_out = 10
-        #xavier, glorot initialization
-        self.weight1 = np.random.uniform(-np.sqrt(6/(n_in + n_out)), np.sqrt(6/(n_in + n_out)), size=(n_in, n_out)) #size should be 10 vector
-        self.weight2 = np.random.uniform(-np.sqrt(6/(n_in + n_out)), np.sqrt(6/(n_in + n_out)), size=(10, 10)) #for the second layer hidden
-        self.weight3 = np.random.uniform(-np.sqrt(6/(n_in + n_out)), np.sqrt(6/(n_in + n_out)), size=(10, 10)) #size should be 10 vector
-
-        self.bias1 = np.array([0] * n_out, dtype=float).reshape(1, 10)
-        self.bias2 = np.array([0] * n_out, dtype=float).reshape(1, 10)
-        self.bias3 = np.array([0] * n_out, dtype=float).reshape(1, 10)
+        if os.path.exists(MODEL_PATH):
+            data = np.load(MODEL_PATH)
+            self.weight1 = data['weight1']
+            self.weight2 = data['weight2']
+            self.weight3 = data['weight3']
+            self.bias1 = data['bias1']
+            self.bias2 = data['bias2']
+            self.bias3 = data['bias3']
+        else:
+            n_in = 784 #28x 28
+            n_out = 10
+            #xavier, glorot initialization
+            self.weight1 = np.random.uniform(-np.sqrt(6/(n_in + n_out)), np.sqrt(6/(n_in + n_out)), size=(n_in, n_out)) #size should be 10 vector
+            self.weight2 = np.random.uniform(-np.sqrt(6/(n_in + n_out)), np.sqrt(6/(n_in + n_out)), size=(10, 10)) #for the second layer hidden
+            self.weight3 = np.random.uniform(-np.sqrt(6/(n_in + n_out)), np.sqrt(6/(n_in + n_out)), size=(10, 10)) #size should be 10 vector
+    
+            self.bias1 = np.array([0] * n_out, dtype=float).reshape(1, 10)
+            self.bias2 = np.array([0] * n_out, dtype=float).reshape(1, 10)
+            self.bias3 = np.array([0] * n_out, dtype=float).reshape(1, 10)
         
     def sigmoid(self, x):
         return 1/(1+math.e**(-x))
@@ -81,8 +89,10 @@ class ANN():
                 self.bias1 -= rate * de_db1
                 self.bias2 -= rate * de_db2
                 self.bias3 -= rate * de_db3
+                
             epoch_losses.append(epoch_loss / len(data))
-            correct =0
+            correct = 0
+            
             for i in range(y_test.shape[0]):
                 rgb = x_test[i].reshape(1, 784)
                 value = y_test[i]
@@ -90,6 +100,14 @@ class ANN():
                 if prediction == value:
                     correct += 1
             print(f"Epoch {epoch+1}, Avg Loss: {epoch_loss / len(data):.5f}, Current accuracy: {correct / y_test.shape[0]}") #avg loss across all data
+
+        np.savez(MODEL_PATH,
+         weight1=self.weight1,
+         bias1=self.bias1,
+         weight2=self.weight2,
+         bias2=self.bias2,
+         weight3=self.weight3,
+         bias3=self.bias3)
         return epoch_losses
     
     def retest(self, input):
@@ -108,49 +126,50 @@ accuracy = []
 network = ANN()
 data = {}
 size = -1
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-train_ds, test_ds = tfds.load(
-    'emnist/digits',           # Dataset name (emnist/digits, emnist/letters, etc.)
-    split=['train', 'test'],   # Load both train and test splits
-    as_supervised=True,        # Returns (image, label) tuples
-    shuffle_files=True,       # Shuffle the data
-)
-x_t, y_t = [], []
-x_te, y_te = [], []
-for image, label in train_ds:
-    x_t.append(image.numpy())
-    y_t.append(label.numpy())
-
-for image, label in test_ds:
-    x_te.append(image.numpy())
-    y_te.append(label.numpy())
-
-x_t = np.array([i.T for i in np.squeeze(np.array(x_t))])
-x_te = np.array([i.T for i in np.squeeze(np.array(x_te))])
-y_te = np.squeeze(np.array(y_te))
-y_t = np.array(y_t)
-
-x_train = np.concatenate((x_train, x_t), axis =0)
-x_test = np.concatenate((x_test, x_te), axis =0)
-y_train = np.concatenate((y_train, y_t), axis =0)
-y_test = np.concatenate((y_test, y_te), axis =0)
-#to gray scale
-x_train = x_train / 255.0
-x_test = x_test / 255.0
-
-for i in range(y_train.shape[0]):
-    rgb = x_train[i].reshape(1, 784)
-    desired_output = [0] * 10
-    desired_output[y_train[i]] = 1
-    desired_output = np.array(desired_output).reshape(1, 10)
-    size += 1
-    data[size] = (rgb, desired_output)
-entries = [i for i in data.keys()]
-#hsuffle before train
-random.shuffle(entries)
-print("Training...")
-avgloss = network.train(data, entries, epochs=150)
-print("Completed!")
+if not os.path.exists(MODEL_PATH): #only train when parameters was not saved or have not been trained before
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    train_ds, test_ds = tfds.load(
+        'emnist/digits',           # Dataset name (emnist/digits, emnist/letters, etc.)
+        split=['train', 'test'],   # Load both train and test splits
+        as_supervised=True,        # Returns (image, label) tuples
+        shuffle_files=True,       # Shuffle the data
+    )
+    x_t, y_t = [], []
+    x_te, y_te = [], []
+    for image, label in train_ds:
+        x_t.append(image.numpy())
+        y_t.append(label.numpy())
+    
+    for image, label in test_ds:
+        x_te.append(image.numpy())
+        y_te.append(label.numpy())
+    
+    x_t = np.array([i.T for i in np.squeeze(np.array(x_t))])
+    x_te = np.array([i.T for i in np.squeeze(np.array(x_te))])
+    y_te = np.squeeze(np.array(y_te))
+    y_t = np.array(y_t)
+    
+    x_train = np.concatenate((x_train, x_t), axis =0)
+    x_test = np.concatenate((x_test, x_te), axis =0)
+    y_train = np.concatenate((y_train, y_t), axis =0)
+    y_test = np.concatenate((y_test, y_te), axis =0)
+    #to gray scale
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    
+    for i in range(y_train.shape[0]):
+        rgb = x_train[i].reshape(1, 784)
+        desired_output = [0] * 10
+        desired_output[y_train[i]] = 1
+        desired_output = np.array(desired_output).reshape(1, 10)
+        size += 1
+        data[size] = (rgb, desired_output)
+    entries = [i for i in data.keys()]
+    #hsuffle before train
+    random.shuffle(entries)
+    print("Training...")
+    avgloss = network.train(data, entries, epochs=150)
+    print("Completed!")
 
 '''In order for the digit to match the centered data in mnist dataset we train on, when processing the digit from the canvas, the image processed
 will be center for better recognition'''
